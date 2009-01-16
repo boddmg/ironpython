0545d0ce02e0616909477c3c0ee28151c6353a0a
fuzzyman <fuzzyman@4d1502e6-cc8b-11dd-8829-37334faa441c>
2009-01-16 17:24:02 +0000

    Adding project for embedding IronPython in Silverlight.
    Michael

---
+++ silverlight/EmbeddingIronPythonSilverlight/EmbeddingIronPythonSilverlight/Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+using IronPython;
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+
+namespace EmbeddingIronPythonSilverlight
+{
+
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+            string code = @"
+def function(string):
+    return string.upper()";
+            ScriptEngine pe = Python.CreateEngine();
+            ScriptScope scope = pe.CreateScope();
+            ScriptSource source = pe.CreateScriptSourceFromString(code, SourceCodeKind.Statements);
+            source.Execute(scope);
+
+            Func<string, string> func = scope.GetVariable<Func<string, string>>("function");
+
+            string result = func("hello world!");
+            textblock.Text = result;
+        }
+    }
+}
