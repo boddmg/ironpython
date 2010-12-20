a81a6a389e49eb196d4df0255022f97084b7bd3e
fuzzyman <fuzzyman@4d1502e6-cc8b-11dd-8829-37334faa441c>
2010-12-20 15:59:11 +0000

    Update to Gtk version of notify example

--- swallowed_whole/gtk_notify.py
+++ swallowed_whole/gtk_notify.py
@@ -9,6 +9,7 @@ import Gdk
 from System.IO import FileSystemWatcher
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
+MSG = 'Change Type: %s\n\nFull Path: %s'
 
 
 class Main(object):
@@ -25,6 +26,9 @@ class Main(object):
         watcher.EnableRaisingEvents = True
 
     def init_notify_icon(self):
+        self.window = Gtk.Window('Directory watcher')
+        self.window.Hide()
+        
         icon = Gdk.Pixbuf("test.ico")
         self.notify_icon = Gtk.StatusIcon(icon)
         self.notify_icon.Visible = True
@@ -40,10 +44,15 @@ class Main(object):
         context_menu.Popup()
 
     def show_notification(self, sender, event):
-        change_type = str(event.ChangeType)
-        full_path = event.FullPath, 
-        window = Gtk.Window('Change Notification')
-        window.ShowAll()
+        def show_dialog(*args):
+            dialog = Gtk.MessageDialog(
+                self.window, Gtk.DialogFlags.Modal,
+                Gtk.MessageType.Warning, Gtk.ButtonsType.None,
+                MSG % (event.ChangeType, event.FullPath)
+            )
+            dialog.Run()
+            dialog.Destroy()
+        Gtk.Application.Invoke(show_dialog)
 
     def on_exit(self, sender, event):
         self.notify_icon.Visible = False
